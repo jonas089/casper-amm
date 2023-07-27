afaabe2378c0f35e508cb218cd6e42c9b1891169
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-07-27 18:51:51 +0200

    work in progress

--- contractAMM/tests/src/integration_tests.rs
+++ contractAMM/tests/src/integration_tests.rs
@@ -4,13 +4,49 @@ mod test_fixture;
 #[cfg(test)]
 mod tests {
     use crate::test_fixture::{Sender, TestEnv};
-    use blake2::digest::{FixedOutput, consts::{U25, U2}, FixedOutputDirty};
     use casper_contract::unwrap_or_revert::UnwrapOrRevert;
     use casper_types::{Key, U256};
     #[test]
     fn should_install() {
         let mut fixture: TestEnv = TestEnv::new();
         fixture.install();
+    }
+    #[test]
+    fn swap(){
+        // first fund the AMM contract with token A and B.
+        // then fund ali with token A
+        // then approve the contract to spend funds in the name of ali
+        // then call the swap
+        // cep18_contract_hash_TOKEN_[ID]
+        // casper_automated_market_maker
+        let mut fixture: TestEnv = TestEnv::new();
+        fixture.install();
+        let contract_hash = fixture.contract_hash("casper_automated_market_maker");
+        let token0 = fixture.contract_hash("cep18_contract_hash_TOKEN_A");
+        let token1 = fixture.contract_hash("cep18_contract_hash_TOKEN_B");
+        // 1000 tokens
+        fixture.transfer(fixture.zero, Key::from(fixture.ali), U256::from(1000), token0);
+        fixture.transfer(fixture.zero, Key::from(contract_hash), U256::from(1000000), token0);
+        fixture.transfer(fixture.zero, Key::from(contract_hash), U256::from(1000000), token1);
+        
+        let ali_balance: U256 = fixture.balance_of(Key::from(fixture.ali), "cep18_contract_hash_TOKEN_A");
+        assert_eq!(ali_balance, U256::from(1000));
+        //fixture.approve(fixture.ali, Key::from(contract_hash), U256::from(1000u128), token0);
+        //fixture.swap(fixture.ali, "cep18_contract_hash_TOKEN_A", U256::from(1000u128));
+        
+        fixture.approve(fixture.ali, Key::from(fixture.bob), U256::from(1000), token0);
+        fixture.transfer_from(fixture.bob, Key::from(fixture.zero), Key::from(fixture.ali), U256::from(1000), token0);
+        let ali_balance = fixture.balance_of(Key::from(fixture.ali), "cep18_contract_hash_TOKEN_A");
+        assert_eq!(ali_balance, U256::from(0));
+
+        //let contract_balance_token_a = fixture.balance_of(Key::from(contract_hash), "cep18_contract_hash_TOKEN_A");
+        //assert_eq!(contract_balance_token_a, U256::from(1001000));
+        //fixture.transfer(fixture.ali, Key::from(fixture.bob), U256::from(1000), token0);
+        //fixture.mint(Key::from(fixture.ali), U256::from(1000), token0);
+        //fixture.mint(Key::from(contract_hash), U256::from(1000_000_000_000_000_000_000u128), token0);
+        //fixture.mint(Key::from(contract_hash), U256::from(1000_000_000_000_000_000_000u128), token1);
+        //fixture.approve(fixture.ali, Key::from(contract_hash), U256::from(1000u128), token1);
+        //fixture.swap(fixture.ali, "cep18_contract_hash_TOKEN_A", U256::from(1000u128));
     }
 }
 
--- contractAMM/tests/src/test_fixture.rs
+++ contractAMM/tests/src/test_fixture.rs
@@ -11,29 +11,33 @@ use casper_types::{
     runtime_args, URef, AsymmetricType, CLTyped, ContractHash, Key, PublicKey, RuntimeArgs, U256, U512, Motes, SecretKey, contracts::{NamedKeys}
 };
 use casper_contract::{unwrap_or_revert::UnwrapOrRevert};
-use blake2::{
-    digest::{Update, VariableOutput, consts::U25},
-    VarBlake2b,
-};
-
-
-fn blake2b256(item_key_string: &[u8]) -> Box<[u8]> {
-    let mut hasher = VarBlake2b::new(32).unwrap();
-    hasher.update(item_key_string);
-    hasher.finalize_boxed()
-}
 
 #[derive(Clone, Copy)]
 pub struct Sender(pub AccountHash);
 
 pub struct TestEnv {
     context: WasmTestBuilder<InMemoryGlobalState>,
+    pub zero: AccountHash,
     pub ali: AccountHash,
     pub bob: AccountHash
 }
 
+pub fn make_dictionary_item_key(owner: Key) -> String {
+    let preimage = owner.to_bytes().unwrap();
+    base64::encode(preimage)
+}
+
 impl TestEnv {
     pub fn new() -> TestEnv {
+        // Token zero address
+        let secret_key_zero = SecretKey::ed25519_from_bytes([9u8; 32]).unwrap();
+        let public_key_zero = PublicKey::from(&secret_key_zero);
+        let zero = AccountHash::from(&public_key_zero);
+        let zero_account = GenesisAccount::account(
+            public_key_zero,
+            Motes::new(U512::from(DEFAULT_ACCOUNT_INITIAL_BALANCE)),
+            None,
+        );
         // Account "ali"
         let secret_key_ali = SecretKey::ed25519_from_bytes([7u8; 32]).unwrap();
         let public_key_ali = PublicKey::from(&secret_key_ali);
@@ -55,6 +59,7 @@ impl TestEnv {
         let mut genesis_config = DEFAULT_GENESIS_CONFIG.clone();
         genesis_config.ee_config_mut().push_account(ali_account);
         genesis_config.ee_config_mut().push_account(bob_account);
+        genesis_config.ee_config_mut().push_account(zero_account);
         let run_genesis_request = RunGenesisRequest::new(
             *DEFAULT_GENESIS_CONFIG_HASH,
             genesis_config.protocol_version(),
@@ -64,8 +69,9 @@ impl TestEnv {
         context.run_genesis(&run_genesis_request).commit();
         TestEnv {
             context,
-            ali,
-            bob
+            zero,
+            bob,
+            ali
         }
     }
 
@@ -75,20 +81,53 @@ impl TestEnv {
 
     pub fn named_keys(&self) -> NamedKeys {
         self.context
-            .get_expected_account(self.default_account())
+            .get_expected_account(self.zero)
             .named_keys()
             .clone()
     }
 
     pub fn contract_hash(&self, name: &str) -> ContractHash{
         self.context
-            .get_expected_account(self.default_account())
+            .get_expected_account(self.zero)
             .named_keys()
             .get(name)
             .expect("must have contract hash key as part of contract creation")
             .into_hash()
             .map(ContractHash::new)
             .expect("must get contract hash")
+    }
+
+    pub fn contract_key(&self, name: &str) -> Key{
+        let contract_hash = self.contract_hash(name);
+        Key::from(contract_hash)
+    }
+
+    pub fn seed_uref_default_account(&self, name: &str) -> URef{
+        let default_account_key: Key = Key::from(self.zero);
+        *self.context
+            .query(None, default_account_key, &[])
+            .expect("contract exists")
+            .as_account()
+            .expect("convert contract")
+            .named_keys()
+            .get(name)
+            .expect("must have key")
+            .as_uref()
+            .expect("must convert to seed uref")
+    }
+
+    pub fn seed_uref_contract(&self, name: &str, contract_name: &str) -> URef {
+        let contract_key: Key = self.contract_key(contract_name);
+        *self.context
+            .query(None, contract_key, &[])
+            .expect("contract exists")
+            .as_contract()
+            .expect("convert contract")
+            .named_keys()
+            .get(name)
+            .expect("must have key")
+            .as_uref()
+            .expect("must convert to seed uref")
     }
 
     pub fn install(&mut self){
@@ -98,30 +137,30 @@ impl TestEnv {
             "symbol" => "ATKN".to_string(),
             "decimals" => u8::from(18),
             // 1000_000 Tokens (1 * 10 ** 24 WEI)
-            "total_supply" => U256::from(1_000_000_000_000_000_000_000_000u128),
+            "total_supply" => U256::from(1000_000_000_000_000_000_000_000u128),
         };
         let session_args_b = runtime_args! {
             // initialise cep18
             "name" => "TOKEN_B".to_string(),
             "symbol" => "BTKN".to_string(),
             "decimals" => u8::from(18),
-            "total_supply" => U256::from(1_000_000_000_000_000_000_000_000u128),
+            "total_supply" => U256::from(1000_000_000_000_000_000_000_000u128),
         };
         let session_args_c = runtime_args! {
             // initialise cep18
             "name" => "TOKEN_C".to_string(),
             "symbol" => "CTKN".to_string(),
             "decimals" => u8::from(18),
-            "total_supply" => U256::from(1_000_000_000_000_000_000_000_000u128),
+            "total_supply" => U256::from(1000_000_000_000_000_000_000_000u128),
         };
         let a_exec_request: casper_execution_engine::core::engine_state::ExecuteRequest =
-            ExecuteRequestBuilder::standard(self.ali, "../wasm/cep18.wasm", session_args_a)
+            ExecuteRequestBuilder::standard(self.zero, "../wasm/cep18.wasm", session_args_a)
             .build();
         let b_exec_request: casper_execution_engine::core::engine_state::ExecuteRequest = 
-            ExecuteRequestBuilder::standard(self.ali, "../wasm/cep18.wasm", session_args_b)
+            ExecuteRequestBuilder::standard(self.zero, "../wasm/cep18.wasm", session_args_b)
             .build();
         let c_exec_request: casper_execution_engine::core::engine_state::ExecuteRequest =
-            ExecuteRequestBuilder::standard(self.ali, "../wasm/cep18.wasm", session_args_c)
+            ExecuteRequestBuilder::standard(self.zero, "../wasm/cep18.wasm", session_args_c)
             .build();
         self.context
             .exec(a_exec_request)
@@ -135,16 +174,18 @@ impl TestEnv {
             .exec(c_exec_request)
             .expect_success()
             .commit();
+
         let a_contract_hash = self.contract_hash("cep18_contract_hash_TOKEN_A");
         let b_contract_hash = self.contract_hash("cep18_contract_hash_TOKEN_B");
         let c_contract_hash = self.contract_hash("cep18_contract_hash_TOKEN_C");
+
         let session_args = runtime_args! {
             "token0" => a_contract_hash,
             "token1" => b_contract_hash,
             "token" => c_contract_hash
         };
         let contract_exec_request: casper_execution_engine::core::engine_state::ExecuteRequest = 
-            ExecuteRequestBuilder::standard(self.ali, "../wasm/swap0.wasm", session_args)
+            ExecuteRequestBuilder::standard(self.zero, "../wasm/swap0.wasm", session_args)
             .build();
         self.context
             .exec(contract_exec_request)
@@ -152,14 +193,13 @@ impl TestEnv {
             .commit();
     }
     
-    pub fn mint(&mut self, owner: Key, amount: U256, contract_name: &str){
-        let contract_hash = self.contract_hash(contract_name);
+    pub fn mint(&mut self, owner: Key, amount: U256, contract_hash: ContractHash){
         let session_args = runtime_args!{
             "owner" => owner,
             "amount" => amount
         };
         let mint_request = ExecuteRequestBuilder::contract_call_by_hash(
-            self.default_account(),
+            self.zero,
             contract_hash,
             "mint",
             session_args
@@ -170,10 +210,7 @@ impl TestEnv {
             .commit();
     }
 
-    pub fn transfer(&mut self, msg_sender: Key, recipient: Key, amount: U256, contract_name: &str){
-        //let a_contract_hash = self.contract_hash("cep18_contract_hash_TOKEN_A");
-        //let b_contract_hash = self.contract_hash("cep18_contract_hash_TOKEN_B");
-        let contract_hash = self.contract_hash(contract_name);
+    pub fn transfer(&mut self, msg_sender: AccountHash, recipient: Key, amount: U256, contract_hash: ContractHash){
         let session_args = runtime_args!{
             "recipient" => recipient,
             "amount" => amount
@@ -191,8 +228,26 @@ impl TestEnv {
             .commit();
     }
 
-    pub fn approve(&mut self, msg_sender: Key, spender: Key, amount: U256, contract_name: &str){
-        let contract_hash = self.contract_hash(contract_name);
+    pub fn transfer_from(&mut self, msg_sender: AccountHash, recipient: Key, owner: Key, amount: U256, contract_hash: ContractHash){
+        let session_args = runtime_args!{
+            "recipient" => recipient,
+            "owner" => owner,
+            "amount" => amount
+        };
+
+        let transfer_request = ExecuteRequestBuilder::contract_call_by_hash(
+            msg_sender,
+            contract_hash,
+            "transfer_from",
+            session_args
+        ).build();
+
+        self.context
+            .exec(transfer_request)
+            .commit();
+    }
+
+    pub fn approve(&mut self, msg_sender: AccountHash, spender: Key, amount: U256, contract_hash: ContractHash){
         let session_args = runtime_args! {
             "spender" => spender,
             "amount" => amount
@@ -210,7 +265,7 @@ impl TestEnv {
             .commit();
     }
 
-    pub fn swap(&mut self, msg_sender: Key, from_token: &str, amount: U256){
+    pub fn swap(&mut self, msg_sender: AccountHash, from_token: &str, amount: U256){
         let contract_hash = self.contract_hash("casper_automated_market_maker");
         let fromToken = self.contract_hash(from_token);
         let session_args = runtime_args! {
@@ -228,5 +283,17 @@ impl TestEnv {
         self.context
             .exec(swap_request)
             .commit();
+    }
+
+    pub fn balance_of(&self, account: Key, contract_name: &str) -> U256{
+        let seed_uref = self.seed_uref_contract("balances", contract_name);
+        let dictionary_key = make_dictionary_item_key(account);
+        self.context.query_dictionary_item(None, seed_uref, &dictionary_key)
+            .unwrap()
+            .as_cl_value()
+            .unwrap()
+            .clone()
+            .into_t()
+            .unwrap()
     }
 }
